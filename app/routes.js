feec9f2012102c72e599ead8636f080c35e08d58
Robert Long <force.distance@gmail.com>
2014-09-27 17:18:21 -0400

    Fixing the css so the label, textbox, and buttons show up on the same row

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ var dbo = new db.connection(dbCreds);
 
 module.exports = function(app) {
     app.get('/api/students', function(req, res) {
-        dbo.connection.query('select * from Students', function(err, rows, fields) {
+        dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
             if (rows.length > 0) {
                 res.json(rows);
             }
@@ -19,7 +19,7 @@ module.exports = function(app) {
     app.post('/api/students/:studentName', function(req, res) {
         var queryString = 'insert into ' + dbo.connection.config.database + '.Students (studentName) values (' + dbo.connection.escape(req.params.studentName) + ')';
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students', function(err, rows, fields) {
+            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
                 if (rows.length > 0) {
                     res.json(rows);
                 }
@@ -34,7 +34,7 @@ module.exports = function(app) {
     app.delete('/api/students/:studentID', function(req, res) {
         var queryString = 'delete from ' + dbo.connection.config.database + '.Students where studentID = ' + dbo.connection.escape(req.params.studentID);
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students', function(err, rows, fields) {
+            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
                 if (rows.length > 0) {
                     res.json(rows);
                 }
@@ -50,7 +50,7 @@ module.exports = function(app) {
     app.put('/api/students/:studentID/:newStudentName', function(req, res) {
         var queryString = 'update ' + dbo.connection.config.database + '.Students set studentName = ' + dbo.connection.escape(req.params.newStudentName) + ' where studentID = '+ dbo.connection.escape(req.params.studentID);
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students', function(err, rows, fields) {
+            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
                 if (rows.length > 0) {
                     res.json(rows);
                 }
