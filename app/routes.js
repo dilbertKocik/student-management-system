344bc16d5357edc747b0db0b26ef25ffc83aa34b
Robert Long <force.distance@gmail.com>
2014-09-27 21:00:16 -0400

    reducing duplicated code

--- app/routes.js
+++ app/routes.js
@@ -1,48 +1,36 @@
 var db = require('./database.js');
 var dbCreds = require('./credentials.json');
+var path = require('path');
 
 var dbo = new db.connection(dbCreds);
 
+var getStudents = function(res) {
+    dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
+        if (rows.length > 0) {
+            res.json(rows);
+        }
+        else {
+            res.json([]);
+        }
+    });
+};
+
 module.exports = function(app) {
     app.get('/api/students', function(req, res) {
-        dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
-            if (rows.length > 0) {
-                res.json(rows);
-            }
-            else
-            {
-                res.json([]);
-            }
-        });
+        getStudents(res);
     });
 
     app.post('/api/students/:studentName', function(req, res) {
         var queryString = 'insert into ' + dbo.connection.config.database + '.Students (studentName) values (' + dbo.connection.escape(req.params.studentName) + ')';
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
-                if (rows.length > 0) {
-                    res.json(rows);
-                }
-                else
-                {
-                    res.json([]);
-                }
-            });
+            getStudents(res);
         });
     });
 
     app.delete('/api/students/:studentID', function(req, res) {
         var queryString = 'delete from ' + dbo.connection.config.database + '.Students where studentID = ' + dbo.connection.escape(req.params.studentID);
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
-                if (rows.length > 0) {
-                    res.json(rows);
-                }
-                else
-                {
-                    res.json([]);
-                }
-            });
+            getStudents(res);
         });
 
     });
@@ -50,19 +38,11 @@ module.exports = function(app) {
     app.put('/api/students/:studentID/:newStudentName', function(req, res) {
         var queryString = 'update ' + dbo.connection.config.database + '.Students set studentName = ' + dbo.connection.escape(req.params.newStudentName) + ' where studentID = '+ dbo.connection.escape(req.params.studentID);
         dbo.connection.query(queryString, function(err, rows, fields) {
-            dbo.connection.query('select * from Students order by studentName', function(err, rows, fields) {
-                if (rows.length > 0) {
-                    res.json(rows);
-                }
-                else
-                {
-                    res.json([]);
-                }
-            });
+            getStudents(res);
         });
     });
 
     app.get('*', function(req, res) {
-        res.sendFile('./public/index.html');
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 };
